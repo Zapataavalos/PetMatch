d3b14ec6b56f7ce3dfce43409078254caeac1a84
Vicente <vi.zapataa@duocuc.cl>
2026-05-20 19:54:36 -0400

    Update to test

--- ms-animal-type/src/test/java/com/petmatch/msanimaltype/MsAnimalTypeApplicationTests.java
+++ ms-animal-type/src/test/java/com/petmatch/msanimaltype/MsAnimalTypeApplicationTests.java
@@ -2,9 +2,14 @@ package com.petmatch.msanimaltype;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.petmatch.msanimaltype.repository.AnimalTypeRepository;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class MsAnimalTypeApplicationTests {
+
+    @MockBean AnimalTypeRepository animalTypeRepository;
+
     @Test
     void contextLoads() {}
 }
